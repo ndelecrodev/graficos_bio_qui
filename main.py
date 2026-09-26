6316d96821e870e803f398190576527dda2c1316
Nicolas Delecrode <nicolas17delecrodepf@gmail.com>
2026-09-25 22:08:33 -0300

    fix: updates the build_in_file name param in the main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ time = [4, 8, 12, 16, 20, 24]
 cells = [1000, 5000, 10000, 20000, 35000, 65000]
 
 Graph.build_graph(time, cells, "Graph Etapa 3", "time (h)", "cells", True)
-Graph.build_in_file("Graph Etapa 1")
+Graph.build_in_file("Graph Etapa 3")
 Graph.build_graph(time, Tool.calcular_logs(cells), "Graph Etapa 4", "time (h)", "cells em log", True)
-Graph.build_in_file("Graph Etapa 2")
+Graph.build_in_file("Graph Etapa 4")
 Graph.plotar()
